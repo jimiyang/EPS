61aeb0fa7d6686f3bfd4f890f5f77afcb0d0643e
jimiyang <601285950@qq.com>
2019-04-12 16:10:37 +0800

    up

--- src/api/api.js
+++ src/api/api.js
@@ -35,9 +35,13 @@ function baseInstance(service, params) {
       baseUrl = `${(window.common.getUrl())[3]}/sahp/base/api/gateway.in`; //'http://192.168.5.21:9999/sahp/base/api/gateway.in';
       signParams = {
         service,
-        ...headParams,
+        channel_partner_id: headParams.partner_id,
+        input_charset: 'UTF-8',
+        version: '1.0',
+        platform_merchant_no: window.localStorage.getItem('platform_no'),
         ...params,
       };
+      console.log(signParams);
       form = {
         head: {
           service,
@@ -46,11 +50,12 @@ function baseInstance(service, params) {
           core_merchant_no: window.localStorage.getItem('merchant_code'), //联富通核心商户
           ...getSign(signParams, aes.Decrypt(localStorage.getItem('PKEY'))),
           sign_type: 'MD5',
-          input_charse: 'UTF-8',
+          input_charset: 'UTF-8',
           request_time: window.common.getDate(new Date(), true)
         },
         body: params
       };
+      console.log(form);
       break;
     default:
       baseUrl = `${(window.common.getUrl())[1]}/eps/base/gateway.in`; //'http://192.168.19.118:8000/eps/base/gateway.in';
